688a75d6a6a04283def46d282bf2fd4eb0585f6a
Alexandra <alexandra.p21m@gmail.com>
2017-11-02 14:56:07 +0200

    Update extra.js

--- extra.js
+++ extra.js
@@ -1,9 +1,9 @@
 // 1. get the min and max value from an array using only reduce
 
 const getMinAndMax =  array => array.reduce(  ( acc, value ) => {
-	acc[0] = acc[0] < value ? acc[0] : value;
-    acc[1] = acc[1] > value ? acc[1] : value;
-    return acc;
+    	acc[0] = acc[0] < value ? acc[0] : value;
+    	acc[1] = acc[1] > value ? acc[1] : value;
+    	return acc;
 
 }, [] );
 
@@ -12,7 +12,7 @@ console.log( getMinAndMax( array ) );
 // 2. implement map using reduce
 
 const map = ( array, func ) => array.reduce( ( acc, current ) => {
-    return acc.concat( func( current ) );
+    	return acc.concat( func( current ) );
 }, [] );
 
 const addOne = ( elem ) => elem + 1;
